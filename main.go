60e93001ac8cdaa532fd9c016526b754045cc5ae
liuda <liuda2498@163.com>
2021-08-03 17:06:23 +0800

    第一次发版本

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	_ "AuditInformation/docs"
+	"AuditInformation/internal/audit"
+	Cors "AuditInformation/internal/cors"
+	"github.com/gin-gonic/gin"
+	gs "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+)
+
+// @title 获取审核信息
+// @version 1.0
+// @description 点击信息按钮，获取审核人，意见等信息
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name 有问题请找前端刘娇娇,联系电话：123567890
+// @contact.url http://www.swagger.io/support
+
+func main() {
+	router := gin.Default()
+	router.Use(Cors.Cors()) // 允许使用跨域请求	 全局中间件
+	url := gs.URL("http://10.28.83.123:8863/swagger/doc.json")
+	router.GET("/swagger/*any", gs.WrapHandler(swaggerFiles.Handler,url))
+	router.GET("/Audit", audit.AuInfo)
+	router.Run(":8863")
+}
